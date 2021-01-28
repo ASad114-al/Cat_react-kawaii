5a20f0f06a490af2a6763a26ff5e104ac89358e2
Asad114-al <as3ad.94aa@gmail.com>
2021-01-28 21:29:01 +0100

    css

--- src/Components/DayToNight.js
+++ src/Components/DayToNight.js
@@ -2,16 +2,20 @@ import { useState } from 'react'
 
 const DayToNight  = () => {
  const [isDay, setIsDay]= useState(false)
-/*  const[color,setColor]=useState("red") */
+
+
  const handDayToNight =()=>{
      setIsDay(!isDay )
  }
 
+
     return ( 
-        <div style={{background: isDay ? 'black' : 'white'} }  >
+        <div id='box' style={{background: isDay ? 'black' :  'white'}}  >
 
                 <h1 style={{color:  !isDay ?   'black':'white'}}>{isDay ? "Night" : "Day"}</h1>
-        <button onClick={handDayToNight}>Change to {!isDay ? "Night" : "Day"} </button>
+                <br/>
+                <br/>
+                <button  onClick={handDayToNight}>Change to {!isDay ? "Night" : "Day"} </button>
         </div>
      );
 }
