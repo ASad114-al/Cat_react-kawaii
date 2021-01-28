1d63b8293113dbab801c8b51adb234a406fbb191
Asad114-al <as3ad.94aa@gmail.com>
2021-01-28 21:37:43 +0100

    2css git push

--- src/Components/DayToNight.js
+++ src/Components/DayToNight.js
@@ -15,7 +15,7 @@ const DayToNight  = () => {
                 <h1 style={{color:  !isDay ?   'black':'white'}}>{isDay ? "Night" : "Day"}</h1>
                 <br/>
                 <br/>
-                <button  onClick={handDayToNight}>Change to {!isDay ? "Night" : "Day"} </button>
+                <button style={{background: !isDay ? 'black' :  'white'},{color: 'black'}} onClick={handDayToNight}>Change to {!isDay ? "Night" : "Day"} </button>
         </div>
      );
 }
