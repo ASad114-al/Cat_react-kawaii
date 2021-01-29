270ee00ddd10d43e2b66589bbb445ed30187cc53
Asad114-al <as3ad.94aa@gmail.com>
2021-01-29 10:25:32 +0100

    form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,12 @@
 
 const Form = () => {
     return (  
-        <form name='contact v1' method='post'>
+        <form name='contact v1' method='post' action='/'>
+
             <input type="hidden" name='form-name' value='contact v1'/> <br/>
             <input type="text" name='first-name'  placeholder='first-name'/> <br/>
             <input type="text" name='email' placeholder='Email'/> <br/>
-            <textarea name="message" id="" cols="30" rows="10"></textarea>
+            <textarea name="message" id="" cols="30" rows="10"></textarea> <br/>
             <button type='submit'>Send Message</button>
         </form>
     );
